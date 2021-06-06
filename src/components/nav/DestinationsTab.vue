610a1ec7a56e75896d558fc34ced535acddcdfec
heaventrip <admin@yourheaventrip.com>
2021-06-06 18:07:02 +0200

    changes

--- src/components/nav/DestinationsTab.vue
+++ src/components/nav/DestinationsTab.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="container">
+  <div class="container h-100">
     <!-- <h4 class="nav-head m-0">
       <a href="#header_nav" class="text-reset pr-3"><i class="fas fa-chevron-left"></i></a>
       . destination
     </h4> -->
     <ul class="list-unstyled wrapper-list d-flex flex-wrap">
       <li>
-        <h4 class="wrapper-list-head">{{ franceCountry.name }}</h4>
+        <!-- <h4 class="wrapper-list-head">{{ franceCountry.name }}</h4> -->
         <div v-for="region in franceCountry.regions" :key="region">
           <h5 class="wrapper-list-subhead">{{ region.name }}</h5>
-          <ul class="list-unstyled wrapper-innerlist pb-4">
+          <ul class="list-unstyled wrapper-innerlist pb-5">
             <li v-for="spot in region.spots" :key="spot">
               <a href="#"><i class="fa fa-chevron-right arrow-left"></i>{{ spot.name }}</a>
             </li>
@@ -17,7 +17,7 @@
         </div>
       </li>
       <li v-for="continent in continents" :key="continent">
-        <h4 class="wrapper-list-head">{{ continent.name }}</h4>
+        <!-- <h4 class="wrapper-list-head">{{ continent.name }}</h4> -->
         <div v-for="country in continent.countries" :key="country" :class="[country.name === 'France' ? 'd-none' : '']">
           <h5 class="wrapper-list-subhead">{{ country.name }}</h5>
           <ul class="list-unstyled wrapper-innerlist pb-4">
