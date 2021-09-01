4c8167dbe224ce0705ff886dbda9b16d67706e8a
GeoffroyMuller <geoffroym2@gmail.com>
2021-09-01 20:50:43 +0200

    up signup

--- src/components/connection/PasswordStep1.vue
+++ src/components/connection/PasswordStep1.vue
@@ -1,20 +1,29 @@
 <template>
   <p class="content-head">Assure toi de renseigner un mail valide :</p>
   <form>
-    <input
-      v-model="email"
-      class="form-control modal-input mb-2"
-      type="text"
-      name=""
-      placeholder="Email"
-      :style="[errors.includes('email') && !validEmail(email) ? 'background-color: #fff8f8; border-left: 4px solid red' : '']"
-    />
+    <div class="form-group has-float-label">
+      <input
+        v-model="email"
+        id="login-email"
+        class="form-control mb-2"
+        type="text"
+        name=""
+        autocomplete="off"
+        placeholder=" "
+        :style="[errors.includes('email') && !validEmail(email) ? 'border-bottom: 1px solid red' : '']"
+      />
+      <label for="login-email">Adresse e-mail</label>
+    </div>
+
     <a @click.stop="$emit('clicked-password-retrieved')" class="password-link d-block text-right mb-5" href="#">J’ai retrouvé mon mot de passe</a>
-    <button @click.stop="submitForm" class="btn btn-dark btn-block text-uppercase border-0 rounded-0 modal-btn" :disabled="!formIsFilled">Valider</button>
+    <!--  <button @click.stop="submitForm" class="btn btn-dark btn-block text-uppercase border-0 rounded-0 modal-btn" :disabled="!formIsFilled">Valider</button> -->
+    <Button id="btn-validate" @click="submitForm" text="Valider" px="1.5rem" size=".8rem" height="50px" width="100%" weight="bold" color="white" :disabled="!formIsFilled" />
   </form>
 </template>
 
 <script>
+import Button from '@/components/elements/Button.vue'
+
 export default {
   name: 'PasswordStep1',
   emits: ['filled', 'clicked-password-retrieved'],
@@ -23,6 +32,9 @@ export default {
       email: '',
       errors: []
     }
+  },
+  components: {
+    Button
   },
   computed: {
     formIsFilled() {
@@ -61,6 +73,39 @@ export default {
 </script>
 
 <style scoped>
+#btn-validate:hover {
+  background-color: #d82558;
+}
+.content-head {
+  margin-bottom: 30px;
+}
+.has-float-label .form-control:placeholder-shown:not(:focus) + label {
+  top: 0.15em;
+  font-size: 100%;
+  color: #fff;
+  opacity: 1;
+}
+.has-float-label label::after {
+  display: none;
+}
+.has-float-label label {
+  left: 0;
+}
+.form-control {
+  padding-left: 0;
+  border-radius: 0;
+  border: none;
+  border-bottom: 1px solid #ffffff33;
+  background-color: transparent;
+  padding-bottom: 0.1rem;
+  color: #fff;
+}
+.form-control:focus {
+  background-color: transparent;
+  border-radius: 0;
+  border: none;
+  border-bottom: 1px solid white;
+}
 button:hover {
   background-color: #d82558 !important;
 }
--- src/components/connection/PasswordStep2.vue
+++ src/components/connection/PasswordStep2.vue
@@ -1,20 +1,28 @@
 <template>
   <img class="user_icon d-block mx-auto" fluid :src="require('@/assets/images/mic.png')" />
-  <h6 class="info-text text-uppercase mt-3">
-    <strong class="text-danger d-block">Un email a été envoyé.</strong>
+  <h6 class="info-text text-uppercase mt-3 mb-5">
+    <strong class="text-danger d-block mb-3">Un email a été envoyé.</strong>
     Il Contient le lien Pour réinitialiser ton mot de passe
   </h6>
-  <button @click.prevent="$emit('filled')" class="btn btn-dark btn-block text-uppercase border-0 rounded-0 modal-btn sent">Renvoyer l'email</button>
+  <Button id="btn-validate" @click="$emit('filled')" text="Renvoyer l'email" px="1.5rem" size=".8rem" height="50px" width="100%" weight="bold" color="white" :disabled="!formIsFilled" />
 </template>
 
 <script>
+import Button from '@/components/elements/Button.vue'
+
 export default {
   name: 'PasswordStep2',
-  emits: ['filled']
+  emits: ['filled'],
+  components: {
+    Button
+  }
 }
 </script>
 
 <style scoped>
+#btn-validate:hover {
+  background-color: #d82558;
+}
 button:hover {
   background-color: #d82558 !important;
 }
